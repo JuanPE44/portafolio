9159717c0d61f33383f4c5749fdce6d1ea21057b
JuanpeDeveloper <juantandil123@gmail.com>
2025-02-03 19:20:57 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import { fileURLToPath, URL } from "node:url";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -11,16 +10,4 @@ export default defineConfig({
   integrations: [tailwind()],
   adapter: vercel(),
   output: "server",
-  vite: {
-    resolve: {
-      alias: {
-        "@components": fileURLToPath(
-          new URL("./src/components", import.meta.url)
-        ),
-        "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
-        "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
-        "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
-      },
-    },
-  },
 });
