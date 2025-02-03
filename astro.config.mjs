2adc140c6e9b1930849332726ee68da5c7be4b68
JuanpeDeveloper <juantandil123@gmail.com>
2025-02-03 01:04:15 -0300

    alias de las rutas agregado a astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    resolve: {
+      alias: {
+        "@components": "./src/components",
+        "@styles": "./src/styles",
+        "@utils": "./src/utils",
+        "@assets": "./src/assets",
+      },
+    },
+  },
 });
