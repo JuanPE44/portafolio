ec016395a5603a66ae424b0cf2d97d37f8cea6b9
JuanpeDeveloper <juantandil123@gmail.com>
2025-02-03 18:13:04 -0300

    integracion con netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: vercel(),
-  vite: {
-    resolve: {
-      alias: {
-        "@components": "/src/components",
-        "@styles": "/src/styles",
-        "@utils": "/src/utils",
-        "@assets": "/src/assets",
-      },
-    },
-  },
+  adapter: netlify(),
 });
