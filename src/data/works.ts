2b3b0bb986346d9d5807472fe166818adda137d4
Juan Peñalba <juantandil123@gmail.com>
2025-08-06 13:01:06 -0300

    feat: pagina items

---
+++ src/data/works.ts
@@ -0,0 +1,15 @@
+export const works = [
+  {
+    slug: "mamparas-esmeralda",
+    urlImage: "/images/work-2.png",
+    urlLink: "https://mamparasesmeralda.vercel.app",
+    urlMore: "items/mamparas-esmeralda",
+    title: "Mamparas Esmeralda",
+    jobTitle: "Freelancer",
+    description:
+      "Encargado del diseño y desarrollo del sitio web para Mamparas Esmeralda, donde lideré la planificación del proyecto y realicé el diseño personalizado del sitio. ",
+    details: ["Diseño UX/UI", "Desarrollo Frontend"],
+    tecnologies: ["Astro", "Figma", "Typescript", "Tailwind"],
+  },
+];
+
